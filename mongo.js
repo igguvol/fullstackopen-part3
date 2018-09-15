30663fe59bf9edc4d51d24d26fd329ccb6047f33
astu <astu@kapsi.fi>
2018-09-15 16:27:08 +0300

    mongo.js

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const Person = require('./models/Person');
+const mongoose = require('mongoose')
+
+if ( process.argv.length == 2 )
+{
+
+  Person.find({})
+    .then(person=>{
+      const mapped = person.map( a => ({'name':a.name,'number':a.number,'id':a._id }) );
+      console.log(mapped);
+      mongoose.connection.close()
+    })
+    .catch( error => console.log(error) );
+
+/*  Person.find({})
+    .then(person=>{
+      console.log(person);
+      mongoose.connection.close()
+    })
+    .catch( error => console.log(error) );*/
+}
+else if ( process.argv.length == 4)
+{
+  const tyyppi = new Person( {"name":process.argv[2],"number":process.argv[3]} );
+  tyyppi
+    .save()
+    .then(response => {
+      console.log('note saved!')
+      mongoose.connection.close()
+    })
+    .catch( error => console.log(error) );
+}
+else
+{
+  console.log("usage: node mongo.js name number");
+  process.exit();
+
+}
