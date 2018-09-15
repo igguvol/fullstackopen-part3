53573df6596a0bdeaece8085eb542bcbc8a4b907
astu <astu@kapsi.fi>
2018-09-15 17:37:55 +0300

    code passing lint

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./models/Person');
-const mongoose = require('mongoose')
+const Person = require('./models/Person')
 
 
-
-app.use(express.static('build'));
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.static('build'))
+app.use(cors())
+app.use(bodyParser.json())
 app.use( morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
@@ -22,49 +20,49 @@ app.use( morgan(function (tokens, req, res) {
     tokens['response-time'](req, res), 'ms'
   ].join(' ')
 })
-);
+)
 
 
 app.get('/api/persons', (req, res) => {
   Person.find({})
     .then(person=>{
-      res.json( person.map( a => {console.log(a.format);return a.format} ) );
+      res.json( person.map( a => {console.log(a.format);return a.format} ) )
     })
-    .catch( error => console.log(error) );
-});
+    .catch( error => console.log(error) )
+})
 
 
 app.post('/api/persons', (req, res) => {
-  var d = req.body;
+  var d = req.body
   if ( d.number === undefined || d.number === '' )
   {
-    res.status(400).json( {'error': 'missing number'} );
-    return;
+    res.status(400).json( {'error': 'missing number'} )
+    return
   }
   if (d.name === 'undefined' || d.name === '' )
   {
-    res.status(400).json( {'error': 'missing name'} );
-    return;
+    res.status(400).json( {'error': 'missing name'} )
+    return
   }
   Person
     .find( {name:d.name} )
     .then( a => {
       if ( a.length === 0 )
       {
-        const tyyppi = new Person( {"name":d.name,"number":d.number} );
+        const tyyppi = new Person( {'name':d.name,'number':d.number} )
         tyyppi
           .save()
           .then(v => {
-            res.status(201).json(v.format);
+            res.status(201).json(v.format)
           })
-          .catch( error => console.log(error) );
+          .catch( error => console.log(error) )
       }
       else
       {
-        res.sendStatus(409);
+        res.sendStatus(409)
       }
     } )
-    .catch( e => console.log('________ error: ', e) );
+    .catch( e => console.log('________ error: ', e) )
 
 })
 
@@ -72,40 +70,42 @@ app.get('/api/persons/:id', (req, res) => {
   Person
     .findById( req.params.id )
     .then(person => {
-      res.json( person.format );
+      res.json( person.format )
     })
-    .catch( error => res.status(400).send({ error: 'malformatted id' }) );
-});
+    .catch( error => {console.log(error); res.status(400).send({ error: 'malformatted id' })} )
+})
 
 app.delete('/api/persons/:id', (req, res) => {
   Person
     .findByIdAndRemove(req.params.id)
-    .then(result => {
+    .then( () => {
       res.status(204).end()
     })
     .catch(error => {
+      console.log(error)
       res.status(400).send({ error: 'malformatted id' })
-    });
+    })
 })
 
 app.put('/api/persons/:id', (req, res) => {
   Person
     .findByIdAndUpdate(req.params.id,req.body)
     .then(result => {
-      res.json(result.format);
+      res.json(result.format)
     })
     .catch(error => {
+      console.log(error)
       res.status(400).send({ error: 'malformatted id' })
-    });
+    })
 })
 
 
 app.get('/info', (req, res) => {
   Person.find({})
     .then(person=>{
-      res.send('puhelinluettelossa ' + person.length + ' henkilön tiedot.<br>' + (new Date()) );
+      res.send('puhelinluettelossa ' + person.length + ' henkilön tiedot.<br>' + (new Date()) )
     })
-    .catch( error => console.log(error) );
+    .catch( error => console.log(error) )
 })
 
 
--- mongo.js
+++ mongo.js
@@ -1,38 +1,30 @@
-const Person = require('./models/Person');
+const Person = require('./models/Person')
 const mongoose = require('mongoose')
 
 if ( process.argv.length == 2 )
 {
-
   Person.find({})
     .then(person=>{
-      const mapped = person.map( a => ({'name':a.name,'number':a.number,'id':a._id }) );
-      console.log(mapped);
+      const mapped = person.map( a => ({'name':a.name,'number':a.number,'id':a._id }) )
+      console.log(mapped)
       mongoose.connection.close()
     })
-    .catch( error => console.log(error) );
-
-/*  Person.find({})
-    .then(person=>{
-      console.log(person);
-      mongoose.connection.close()
-    })
-    .catch( error => console.log(error) );*/
+    .catch( error => console.log(error) )
 }
 else if ( process.argv.length == 4)
 {
-  const tyyppi = new Person( {"name":process.argv[2],"number":process.argv[3]} );
+  const tyyppi = new Person( {'name':process.argv[2],'number':process.argv[3]} )
   tyyppi
     .save()
     .then(response => {
-      console.log('note saved!')
+      console.log('note saved: ', response)
       mongoose.connection.close()
     })
-    .catch( error => console.log(error) );
+    .catch( error => console.log(error) )
 }
 else
 {
-  console.log("usage: node mongo.js name number");
-  process.exit();
+  console.log('usage: node mongo.js name number')
+  process.exit()
 
 }
